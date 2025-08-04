5e17bb5633c24cc75b2381dbdce9f2f2a8013572
AndileP3 <mamogobalaleandile@gmail.com>
2025-08-04 16:56:46 +0200

    this commit add a functional island for left and right arrows

--- src/theme/components/islands/FeaturedLocations.jsx
+++ src/theme/components/islands/FeaturedLocations.jsx
@@ -1,80 +1,37 @@
+// src/theme/components/islands/FeaturedLocations.js
+import React, { useRef } from 'react';
 import { ModuleFields, TextField } from '@hubspot/cms-components/fields';
-import { useRef } from 'react';
 import styles from '../../styles/featuredLocations.module.css';
 
-export function Component({ fieldValues }) {
+export default function FeaturedLocationsIsland({ fieldValues }) {
   const carouselRef = useRef();
-
-  // Width of one card + gap
-  const scrollAmount = 316; // 300px card + 16px gap
+  const scrollAmount = 316;
 
   const scrollLeft = () => {
-    if (carouselRef.current) {
-      carouselRef.current.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
-    }
+    carouselRef.current?.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
   };
 
   const scrollRight = () => {
-    if (carouselRef.current) {
-      carouselRef.current.scrollBy({ left: scrollAmount, behavior: 'smooth' });
-    }
+    carouselRef.current?.scrollBy({ left: scrollAmount, behavior: 'smooth' });
   };
 
   const offices = [
-    {
-      title: fieldValues.office1_title || "Manchester",
-      location: fieldValues.office1_location || "Balloon Street",
-      img: "https://www.orega.com/hs-fs/hubfs/Balloon%20Street%20-%20Manchester/balloon-street-serviced-office-manchester.jpg?width=600&height=338&name=balloon-street-serviced-office-manchester.jpg"
-    },
-    {
-      title: fieldValues.office2_title || "London",
-      location: fieldValues.office2_location || "Gracechurch Street",
-      img: "https://www.orega.com/hs-fs/hubfs/Orega_GracechurchSt_Building.jpg?width=600&height=338&name=Orega_GracechurchSt_Building.jpg"
-    },
-    {
-      title: fieldValues.office3_title || "Bristol",
-      location: fieldValues.office3_location || "Bristol City Centre",
-      img: "https://www.orega.com/hs-fs/hubfs/Orega%20Bristol_Exterior_1000x665.jpg?width=600&height=338&name=Orega%20Bristol_Exterior_1000x665.jpg"
-    },
-    {
-      title: fieldValues.office4_title || "Gatwick",
-      location: fieldValues.office4_location || "Gatwick Office",
-      img: "https://www.orega.com/hs-fs/hubfs/gatwick-feature.png?width=600&height=338&name=gatwick-feature.png"
-    },
-    {
-      title: fieldValues.office5_title || "Arkwright House",
-      location: fieldValues.office5_location || "Manchester",
-      img: "https://www.orega.com/hs-fs/hubfs/arkwright-house-feature.png?width=600&height=338&name=arkwright-house-feature.png"
-    }
+    { title: fieldValues.office1_title || "Manchester", location: fieldValues.office1_location || "Balloon Street", img: "https://www.orega.com/hs-fs/hubfs/Balloon%20Street%20-%20Manchester/balloon-street-serviced-office-manchester.jpg?width=600&height=338&name=balloon-street-serviced-office-manchester.jpg" },
+    { title: fieldValues.office2_title || "London", location: fieldValues.office2_location || "Gracechurch Street", img: "https://www.orega.com/hs-fs/hubfs/Orega_GracechurchSt_Building.jpg?width=600&height=338&name=Orega_GracechurchSt_Building.jpg" },
+    { title: fieldValues.office3_title || "Bristol", location: fieldValues.office3_location || "Bristol City Centre", img: "https://www.orega.com/hs-fs/hubfs/Orega%20Bristol_Exterior_1000x665.jpg?width=600&height=338&name=Orega%20Bristol_Exterior_1000x665.jpg" },
+    { title: fieldValues.office4_title || "Gatwick", location: fieldValues.office4_location || "Gatwick Office", img: "https://www.orega.com/hs-fs/hubfs/gatwick-feature.png?width=600&height=338&name=gatwick-feature.png" },
+    { title: fieldValues.office5_title || "Arkwright House", location: fieldValues.office5_location || "Manchester", img: "https://www.orega.com/hs-fs/hubfs/arkwright-house-feature.png?width=600&height=338&name=arkwright-house-feature.png" },
   ];
 
   return (
     <section className={styles.featuredSection}>
       <h2 className={styles.heading}>Featured Locations</h2>
-
       <div className={styles.carouselWrapper}>
-        <button
-          onClick={scrollLeft}
-          className={`${styles.arrowBtn} ${styles.left}`}
-          aria-label="Scroll left"
-          type="button"
-        >
-          &larr;
-        </button>
-
-        <div
-          className={styles.carousel}
-          ref={carouselRef}
-          tabIndex={0}  // make it focusable
-        >
+        <button onClick={scrollLeft} className={`${styles.arrowBtn} ${styles.left}`} aria-label="Scroll left" type="button">&larr;</button>
+        <div className={styles.carousel} ref={carouselRef} tabIndex={0}>
           {offices.map((office, index) => (
             <div className={styles.card} key={index}>
-              <img
-                src={office.img}
-                alt={`${office.title} Office`}
-                className={styles.image}
-                loading="lazy"
-              />
+              <img src={office.img} alt={`${office.title} Office`} className={styles.image} loading="lazy" />
               <div className={styles.cardContent}>
                 <h3>{office.title}</h3>
                 <p className={styles.location}>{office.location}</p>
@@ -86,15 +43,7 @@ export function Component({ fieldValues }) {
             </div>
           ))}
         </div>
-
-        <button
-          onClick={scrollRight}
-          className={`${styles.arrowBtn} ${styles.right}`}
-          aria-label="Scroll right"
-          type="button"
-        >
-          &rarr;
-        </button>
+        <button onClick={scrollRight} className={`${styles.arrowBtn} ${styles.right}`} aria-label="Scroll right" type="button">&rarr;</button>
       </div>
     </section>
   );
@@ -102,18 +51,10 @@ export function Component({ fieldValues }) {
 
 export const fields = (
   <ModuleFields>
-    {[1, 2, 3, 4, 5].map((i) => (
+    {[1, 2, 3, 4, 5].map(i => (
       <div key={i}>
-        <TextField
-          name={`office${i}_title`}
-          label={`Office ${i} Title`}
-          default={["Manchester", "London", "Bristol", "Gatwick", "Arkwright House"][i - 1]}
-        />
-        <TextField
-          name={`office${i}_location`}
-          label={`Office ${i} Location`}
-          default={["Balloon Street", "Gracechurch Street", "Bristol City Centre", "Gatwick Office", "Manchester"][i - 1]}
-        />
+        <TextField name={`office${i}_title`} label={`Office ${i} Title`} default={["Manchester", "London", "Bristol", "Gatwick", "Arkwright House"][i - 1]} />
+        <TextField name={`office${i}_location`} label={`Office ${i} Location`} default={["Balloon Street", "Gracechurch Street", "Bristol City Centre", "Gatwick Office", "Manchester"][i - 1]} />
       </div>
     ))}
   </ModuleFields>
--- src/theme/components/modules/FeaturedL.jsx
+++ src/theme/components/modules/FeaturedL.jsx
@@ -1,18 +1,21 @@
 import { ModuleFields, TextField } from '@hubspot/cms-components/fields';
-import { Component as FeaturedLocationsIsland } from '../islands/FeaturedLocations';
+import {Island} from '@hubspot/cms-components';
+import FeaturedLocationsIsland from '../islands/FeaturedLocations?island';  // <-- notice ?island here
 import styles from '../../styles/featuredLocations.module.css';
 
 export function Component({ fieldValues }) {
   return (
     <div className={styles.carouselWrapper}>
-      <FeaturedLocationsIsland fieldValues={fieldValues} />
+      <Island 
+      module={FeaturedLocationsIsland} 
+      fieldValues={fieldValues} />
     </div>
   );
 }
 
 export const fields = (
   <ModuleFields>
-    {[1, 2, 3, 4, 5].map((i) => (
+    {[1, 2, 3, 4, 5].map(i => (
       <div key={i}>
         <TextField
           name={`office${i}_title`}
