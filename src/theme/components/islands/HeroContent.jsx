4852c0a7358eaf6ed0c24548d5485d20f861f612
AndileP3 <mamogobalaleandile@gmail.com>
2025-08-11 16:58:11 +0200

    add the new data like selected option under button

--- src/theme/components/islands/HeroContent.jsx
+++ src/theme/components/islands/HeroContent.jsx
@@ -1,4 +1,3 @@
-
 import { useState } from 'react';
 import styles from '../../styles/hero.module.css';
 
@@ -9,6 +8,62 @@ export default function HeroContent({ fieldValues }) {
     buttonText = "Find",
   } = fieldValues;
 
+  const offices = {
+    London: [
+      'Strand',
+      'Mark Lane',
+      'Lime Street',
+      'Holborn Gate',
+      'Gracechurch Street',
+      'High Holborn',
+      'Old Bailey'
+    ],
+    Manchester: [
+      'The Tootal Buildings',
+      'Balloon Street',
+      'Manchester Piccadilly',
+      'Arkwright House'
+    ],
+    North: [
+      'Newcastle',
+      'Liverpool',
+      'Leeds'
+    ],
+    Midlands: [
+      'The Colmore Building - Birmingham',
+      'Ingenuity House - Birmingham'
+    ],
+    South: [
+      'Marlow',
+      'Bristol',
+      'Gatwick',
+      'Uxbridge',
+      'Stockley Park'
+    ],
+    Scotland: [
+      'Aberdeen',
+      'Glasgow St Vincent Street'
+    ]
+  };
+
+  const [selectedLocation, setSelectedLocation] = useState('');
+  const [selectedPeople, setSelectedPeople] = useState('');
+  const [selectedService, setSelectedService] = useState('');
+
+  const [hoveredMenu, setHoveredMenu] = useState(null);
+  let hoverTimeout = null;
+
+  const handleMouseEnter = (menu) => {
+    clearTimeout(hoverTimeout);
+    setHoveredMenu(menu);
+  };
+
+  const handleMouseLeave = () => {
+    hoverTimeout = setTimeout(() => {
+      setHoveredMenu(null);
+    }, 500); // 5s delay before hiding
+  };
+
   return (
     <div className={styles.contentContainer}>
       <div className={styles.content}>
@@ -17,76 +72,103 @@ export default function HeroContent({ fieldValues }) {
 
         {/* Dropdown Buttons */}
         <div className={styles.supportContainer}>
+
           {/* Location Dropdown */}
-          <div className={styles.dropdownWrapper}>
+          <div className={styles.dropdownWrapper}
+               onMouseEnter={() => handleMouseEnter('location')}
+               onMouseLeave={handleMouseLeave}>
             <div className={styles.dropdownTriggerWrapper}>
               <button className={styles.dropdownTrigger}>
                 Choose a location
               </button>
-              <div className={styles.dropdownMenu1}>
-                {['London', 'Manchester', 'Birmingham', 'Aberdeen'].map((city) => (
-                  <div key={city} className={styles.dropdownColumn}>
-                    <h4>{city}</h4>
-                    <ul>
-                      <li className={styles.dropdownItem}>Office A</li>
-                      <li className={styles.dropdownItem}>Office B</li>
-                      <li className={styles.dropdownItem}>Office C</li>
-                    </ul>
-                  </div>
-                ))}
-              </div>
+              {selectedLocation && <div className={styles.selectedValue}>{selectedLocation}</div>}
+              {hoveredMenu === 'location' && (
+                <div className={styles.dropdownMenu1}>
+                  {Object.entries(offices).map(([city, officeList]) => (
+                    <div key={city} className={styles.dropdownColumn}>
+                      <h4>{city}</h4>
+                      <ul>
+                        {officeList.map((office) => (
+                          <li
+                            key={office}
+                            className={styles.dropdownItem}
+                            onClick={() => setSelectedLocation(office)}
+                          >
+                            {office}
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
+                  ))}
+                </div>
+              )}
             </div>
           </div>
 
           {/* People Dropdown */}
-          <div className={styles.dropdownWrapper}>
+          <div className={styles.dropdownWrapper}
+               onMouseEnter={() => handleMouseEnter('people')}
+               onMouseLeave={handleMouseLeave}>
             <div className={styles.dropdownTriggerWrapper}>
               <button className={styles.dropdownTrigger}>
                 No. People
               </button>
-              <div className={styles.dropdownMenu2}>
-                <div className={styles.dropdownColumn}>
-                  {['1-5', '6-10', '11-20', '20+'].map((group) => (
-                    <button key={group} className={styles.dropdownItem}>
-                      {group}
-                    </button>
-                  ))}
+              {selectedPeople && <div className={styles.selectedValue}>{selectedPeople}</div>}
+              {hoveredMenu === 'people' && (
+                <div className={styles.dropdownMenu2}>
+                  <div className={styles.dropdownColumn}>
+                    <h4>No. People</h4>
+                    {['1', '2-3', '4-10', '11-20', '21-30', '31-40', '41-50', '50+'].map((group) => (
+                      <button
+                        key={group}
+                        className={styles.dropdownItem}
+                        onClick={() => setSelectedPeople(group)}
+                      >
+                        {group}
+                      </button>
+                    ))}
+                  </div>
                 </div>
-              </div>
+              )}
             </div>
           </div>
 
           {/* Services Dropdown */}
-          <div className={styles.dropdownWrapper}>
+          <div className={styles.dropdownWrapper}
+               onMouseEnter={() => handleMouseEnter('service')}
+               onMouseLeave={handleMouseLeave}>
             <div className={styles.dropdownTriggerWrapper}>
               <button className={styles.dropdownTrigger}>
                 Select a Service
               </button>
-              <div className={styles.dropdownMenu3}>
-                {[
-                  {
-                    label: 'Private Office',
-                    image: 'https://www.orega.com/hubfs/private-office.jpg',
-                  },
-                  {
-                    label: 'Co-Working Space',
-                    image: 'https://www.orega.com/hubfs/coworking.jpg',
-                  },
-                  {
-                    label: 'Meeting Room',
-                    image: 'https://www.orega.com/hubfs/meeting-room.jpg',
-                  },
-                  {
-                    label: 'Virtual Office',
-                    image: 'https://www.orega.com/hubfs/virtual-office.jpg',
-                  },
-                ].map((service) => (
-                  <div key={service.label} className={styles.dropdownServiceCard}>
-                    <img src={service.image} alt={service.label} className={styles.serviceImage} />
-                    <p className={styles.serviceLabel}>{service.label}</p>
-                  </div>
-                ))}
-              </div>
+              {selectedService && <div className={styles.selectedValue}>{selectedService}</div>}
+              {hoveredMenu === 'service' && (
+                <div className={styles.dropdownMenu3}>
+                  {[
+                    {
+                      label: 'Serviced Offices',
+                      image: 'https://www.orega.com/hubfs/Icons/service-offices.svg',
+                    },
+                    {
+                      label: 'Virtual Offices',
+                      image: 'https://www.orega.com/hubfs/virtual-offices-1.svg',
+                    },
+                    {
+                      label: 'Meeting Rooms',
+                      image: 'https://www.orega.com/hubfs/Icons/meeting-rooms.svg',
+                    },
+                  ].map((service) => (
+                    <div
+                      key={service.label}
+                      className={styles.dropdownServiceCard}
+                      onClick={() => setSelectedService(service.label)}
+                    >
+                      <p className={styles.serviceLabel}>{service.label}</p>
+                      <img src={service.image} alt={service.label} className={styles.serviceImage} />
+                    </div>
+                  ))}
+                </div>
+              )}
             </div>
           </div>
 
