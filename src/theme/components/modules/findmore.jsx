cade11de9b640170bc4f6c08a79b61dabb2c448e
AndileP3 <mamogobalaleandile@gmail.com>
2025-08-16 18:31:36 +0200

    temporary heading and text for testing

--- src/theme/components/modules/findmore.jsx
+++ src/theme/components/modules/findmore.jsx
@@ -5,7 +5,13 @@ export function Component({ fieldValues }) {
   const { heading, message, buttonText } = fieldValues;
 
   return (
-    <section className={styles.findMoreSection}>
+    <>
+      <div className={styles.inlineContainer}>
+        <h3>Heading:</h3>
+        <p>This is a paragraph next to the heading.</p>
+      </div>
+      
+      <section className={styles.findMoreSection}>
       <div className={styles.videoWrapper}>
         <video
           className={styles.video}
@@ -22,12 +28,13 @@ export function Component({ fieldValues }) {
         <button className={styles.button}>{buttonText}</button>
       </div>
     </section>
+    </>
   );
 }
 
 export const fields = (
   <ModuleFields>
-    <TextField name="heading" label="Heading" default="Don’t lose your company culture." />
+    <TextField name="heading" label="Heading" default=" lose your company culture." />
     <TextField name="message" label="Message" default="At Orega, we believe that great things happen when people come together. So we create beautiful office space and deliver unbeatable service to help you unlock the full potential of your business." />
     <TextField name="buttonText" label="Button Text" default="Find out more" />
   </ModuleFields>
