d83e002bd155616e95f5efe2112c487414e6236f
AndileP3 <mamogobalaleandile@gmail.com>
2025-08-18 12:39:10 +0200

    this commit add a module fild on the backgroud image to be replaceble

--- src/theme/components/modules/newoffice.jsx
+++ src/theme/components/modules/newoffice.jsx
@@ -1,14 +1,20 @@
-import { ModuleFields, TextField } from '@hubspot/cms-components/fields';
+import { ModuleFields, TextField, ImageField } from '@hubspot/cms-components/fields';
 import styles from '../../styles/newoffice.module.css';
 
+// Default background image URL
+const DEFAULT_BG_URL = 'https://2947606.fs1.hubspotusercontent-na1.net/hub/2947606/hubfs/OregaEightyStrand_BreakoutSpace_Courtyard-1.jpg?width=2000&height=1125&name=OregaEightyStrand_BreakoutSpace_Courtyard-1.jpg';
+
 export function Component({ fieldValues }) {
-  const { heading, message, buttonText } = fieldValues;
+  const { heading, message, buttonText, backgroundImage } = fieldValues;
+
+  // Use editor-selected image if available, otherwise fallback to default
+  const bgImageUrl = backgroundImage?.src || DEFAULT_BG_URL;
 
   return (
     <section
       className={styles.newOfficeSection}
       style={{
-        backgroundImage: `url('https://2947606.fs1.hubspotusercontent-na1.net/hub/2947606/hubfs/OregaEightyStrand_BreakoutSpace_Courtyard-1.jpg?width=2000&height=1125&name=OregaEightyStrand_BreakoutSpace_Courtyard-1.jpg')`
+        backgroundImage: `url('${bgImageUrl}')`,
       }}
     >
       <div className={styles.overlayBox}>
@@ -29,6 +35,7 @@ export const fields = (
       default="Located on the Strand, close to Covent Garden, 80 Strand London is a stunning grade II listed building with beautiful art deco features. Eighty Strand is located at one of our most premium addresses in London. Home to some of the UK’s leading brands, 80 Strand London is an ideal location to base your businesses office space."
     />
     <TextField name="buttonText" label="Button Text" default="Find out more" />
+    <ImageField name="backgroundImage" label="Background Image" />
   </ModuleFields>
 );
 
