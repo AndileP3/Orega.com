8ac6c670b56185a11cac1b103d4cf83e28e2d41c
AndileP3 <mamogobalaleandile@gmail.com>
2025-08-18 12:35:24 +0200

    this commit add default media files,

--- src/theme/components/modules/mediatext.jsx
+++ src/theme/components/modules/mediatext.jsx
@@ -1,13 +1,16 @@
-import { ModuleFields, TextField, VideoField, ImageField } from '@hubspot/cms-components/fields';
+import { ModuleFields, TextField, ImageField, VideoField } from '@hubspot/cms-components/fields';
 import styles from '../../styles/mediaTextSections.module.css';
 
-export function Component({ fieldValues }) {
-  const basePath = '../../assets/';
+// Full URLs for your uploaded media files on HubSpot
+const SECTION1_IMAGE_URL = 'https://47574277.fs1.hubspotusercontent-na1.net/hubfs/47574277/Orega.com%20-%20media%20files/mediatext-img1.webp';
+const SECTION2_VIDEO_URL = 'https://47574277.fs1.hubspotusercontent-na1.net/hubfs/47574277/Orega.com%20-%20media%20files/video2.mp4';
+const SECTION3_IMAGE_URL = 'https://47574277.fs1.hubspotusercontent-na1.net/hubfs/47574277/mediatext-img2.webp';
 
+export function Component({ fieldValues }) {
   const sections = [
     {
       mediaType: 'image',
-      mediaSrc: basePath + fieldValues.section1_media,
+      mediaSrc: fieldValues.section1_media?.src || SECTION1_IMAGE_URL,
       title: fieldValues.section1_title,
       description: fieldValues.section1_description,
       button: fieldValues.section1_button,
@@ -15,7 +18,7 @@ export function Component({ fieldValues }) {
     },
     {
       mediaType: 'video',
-      mediaSrc: basePath + fieldValues.section2_media,
+      mediaSrc: fieldValues.section2_media?.src || SECTION2_VIDEO_URL,
       title: fieldValues.section2_title,
       description: fieldValues.section2_description,
       button: fieldValues.section2_button,
@@ -23,7 +26,7 @@ export function Component({ fieldValues }) {
     },
     {
       mediaType: 'image',
-      mediaSrc: basePath + fieldValues.section3_media,
+      mediaSrc: fieldValues.section3_media?.src || SECTION3_IMAGE_URL,
       title: fieldValues.section3_title,
       description: fieldValues.section3_description,
       button: fieldValues.section3_button,
@@ -56,28 +59,26 @@ export function Component({ fieldValues }) {
   );
 }
 
-
 export const fields = (
-<ModuleFields>
-  {/* Section 1 - Image */}
-  <TextField name="section1_title" label="Section 1 Title" default="It is your office space, not ours." />
-  <TextField name="section1_description" label="Section 1 Description" default="You won’t find our logo above the door in our flexible offices. Our bespoke design service will boost your brand and inspire your team." />
-  <TextField name="section1_button" label="Section 1 Button" default="Find out more" />
-  <TextField name="section1_media" label="Section 1 Image Filename" default="mediatext-img1.webp" />
+  <ModuleFields>
+    {/* Section 1 - Image */}
+    <TextField name="section1_title" label="Section 1 Title" default="It's your office space, not ours." />
+    <TextField name="section1_description" label="Section 1 Description" default="You won't find our logo above the door in our flexible offices. Our bespoke design service will boost your brand and inspire your team." />
+    <TextField name="section1_button" label="Section 1 Button" default="Find out more" />
+    <ImageField name="section1_media" label="Section 1 Image" />
 
-  {/* Section 2 - Video */}
-  <TextField name="section2_title" label="Section 2 Title" default="Thoughtfully designed workspace" />
-  <TextField name="section2_description" label="Section 2 Description" default="Our serviced offices aren’t just expertly designed, they’re well thought-out too. Do your best work in sound-proofed private offices with more desk space per person than the industry norm." />
-  <TextField name="section2_button" label="Section 2 Button" default="Book a viewing" />
-  <TextField name="section2_media" label="Section 2 Video Filename" default="video2.mp4" />
+    {/* Section 2 - Video */}
+    <TextField name="section2_title" label="Section 2 Title" default="Thoughtfully designed workspace" />
+    <TextField name="section2_description" label="Section 2 Description" default="Our serviced offices aren't just expertly designed, they're well thought-out too. Do your best work in sound-proofed private offices with more desk space per person than the industry norm." />
+    <TextField name="section2_button" label="Section 2 Button" default="Book a viewing" />
+    <VideoField name="section2_media" label="Section 2 Video" />
 
-  {/* Section 3 - Image */}
-  <TextField name="section3_title" label="Section 3 Title" default="On-site concierge service" />
-  <TextField name="section3_description" label="Section 3 Description" default="We offer second-to-none hospitality from brilliant people with a passion for delivering a business-class experience. Impress your guests and give your teams a VIP experience they won’t forget." />
-  <TextField name="section3_button" label="Section 3 Button" default="Explore more" />
-  <TextField name="section3_media" label="Section 3 Image Filename" default="mediatext-img2.webp" />
-</ModuleFields>
-
+    {/* Section 3 - Image */}
+    <TextField name="section3_title" label="Section 3 Title" default="On-site concierge service" />
+    <TextField name="section3_description" label="Section 3 Description" default="We offer second-to-none hospitality from brilliant people with a passion for delivering a business-class experience. Impress your guests and give your teams a VIP experience they won't forget." />
+    <TextField name="section3_button" label="Section 3 Button" default="Explore more" />
+    <ImageField name="section3_media" label="Section 3 Image" />
+  </ModuleFields>
 );
 
 export const meta = {
